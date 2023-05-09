9dbb28b2a4e7ec8398a2914064f639a5d5576ffa
David SPORN <sporniket.studio@gmail.com>
2023-05-09 06:57:54 +0200

    update copyright notices

--- lib/Tm1637/include/MessageBuilderForTm1637.hpp
+++ lib/Tm1637/include/MessageBuilderForTm1637.hpp
@@ -1,19 +1,19 @@
 // Copyright 2023 David SPORN
 // ---
-// This file is part of 'Tm1637Esp32-by-sporniket'.
+// This file is part of 'Tm1637-by-sporniket'.
 // ---
-// 'Tm1637Esp32-by-sporniket' is free software: you can redistribute it and/or 
+// 'Tm1637-by-sporniket' is free software: you can redistribute it and/or 
 // modify it under the terms of the GNU General Public License as published 
 // by the Free Software Foundation, either version 3 of the License, or 
 // (at your option) any later version.
 
-// 'Tm1637Esp32-by-sporniket' is distributed in the hope that it will be useful, 
+// 'Tm1637-by-sporniket' is distributed in the hope that it will be useful, 
 // but WITHOUT ANY WARRANTY; without even the implied warranty of 
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General 
 // Public License for more details.
 
 // You should have received a copy of the GNU General Public License along 
-// with 'Tm1637Esp32-by-sporniket'. If not, see <https://www.gnu.org/licenses/>. 
+// with 'Tm1637-by-sporniket'. If not, see <https://www.gnu.org/licenses/>. 
 
 #ifndef MESSAGE_BUILDER_FOR_TM1637_HPP
 #define MESSAGE_BUILDER_FOR_TM1637_HPP
